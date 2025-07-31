4561de2d4f2f1ee549daf33e10c8da4f65816893
DELL <DELL@DESKTOP-CL56SIP>
2025-07-31 22:16:03 +0530

    first commit

---
+++ newjava/src/com/newjava/Student.java
@@ -0,0 +1,9 @@
+ package com.newjava;
+
+public class Student {
+	public static void main(String[] args)
+	{
+		System.out.println("chhtrapati shivaji Maharaj");
+	}
+
+}
---
+++ newjava/src/com/newjava/firstprogram.java
@@ -0,0 +1,9 @@
+package com.newjava;
+
+public class firstprogram {
+	public static void main(String[] args) {
+		System.out.println("-----------Aniket Kakade------");
+		
+	}
+
+}
