e08a88b8eb42dc8f4897639282b88def0ff1b1b8
DELL <DELL@DESKTOP-CL56SIP>
2025-08-01 19:28:57 +0530

    radius

---
+++ demo/src/com/demo/radiuscalculate.java
@@ -0,0 +1,10 @@
+package com.demo;
+
+public class radiuscalculate {
+	public static void main(String[] args) {
+		int radius=10;
+		double area=3.14*radius*radius;
+		System.out.println(area);
+	}
+
+}
