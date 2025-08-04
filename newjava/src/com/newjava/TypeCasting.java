bdf8cc1efa14e125b57b95187e9670c8e1791641
DELL <DELL@DESKTOP-CL56SIP>
2025-08-04 12:15:11 +0530

    typecasting

---
+++ newjava/src/com/newjava/TypeCasting.java
@@ -0,0 +1,26 @@
+package com.newjava;
+
+public class TypeCasting {
+	public static void main(String[] args) {
+		//wideing//implicit type casting
+		
+		byte b=10;
+		int l=b;
+		System.out.println(l);
+		float f=22.14f;
+		double d=f;
+		System.out.println(d);
+		char ch='A';
+		int i=ch;
+		System.out.println(ch);
+		double f1=10.52f;
+		long ll=(long) f1;
+		System.out.println(ll);
+		int i2=130;
+		byte b1=(byte) i2;
+		System.out.println(b1);
+		
+		
+	}
+
+}
