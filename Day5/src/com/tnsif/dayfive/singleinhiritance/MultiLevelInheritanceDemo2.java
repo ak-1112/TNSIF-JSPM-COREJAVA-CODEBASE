803869f06d226ecdfb4ab7fc92e421292e2eb76e
DELL <DELL@DESKTOP-CL56SIP>
2025-09-10 08:09:41 +0530

    mul

---
+++ Day5/src/com/tnsif/dayfive/singleinhiritance/MultiLevelInheritanceDemo2.java
@@ -0,0 +1,20 @@
+package com.tnsif.dayfive.singleinhiritance;
+
+
+public class MultiLevelInheritanceDemo2 {
+
+	public static void main(String[] args) {
+		
+		City c=new City();
+		c.setCityName("beed");
+		c.setArea(12345.890f);
+		c.setStateName("maharashtra");
+		c.setLanguage("marathi");
+		c.setCountryname("India");
+		c.setCapital("Delhi");
+		
+		System.out.println(c);
+
+	}
+
+}
